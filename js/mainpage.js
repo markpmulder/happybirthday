3692c1d8879cdc4d19540a22e12c9014af813a71
Mark Mulder <markpmulder@gmail.com>
2021-03-28 17:37:11 +0200

    moved render stuff to index.html

--- js/mainpage.js
+++ js/mainpage.js
@@ -1,47 +1,49 @@
 import * as THREE from './build/three.module.js';
 import { GLTFLoader } from './examples/jsm/loaders/GLTFLoader.js';
+//init scene and camera
 const scene = new THREE.Scene();
-//const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-const camera = new THREE.PerspectiveCamera(60, 1, 1, 1000);
-camera.position.set(0, 0, 500);
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+// const camera = new THREE.PerspectiveCamera(60, 1, 1, 1000);
+// camera.position.set(0, 0, 500);
 
+//init renderer 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-//cat loader
-const loader = new GLTFLoader();
-loader.load( 'assets/cat.glb', function ( gltf ) {
-        scene.add( gltf.scene );
-    }, undefined, function ( error ) {
-        console.error( error );
-} );
+// //cat loader
+// const loader = new GLTFLoader();
+// loader.load( 'assets/cat.glb', function ( gltf ) {
+//         scene.add( gltf.scene );
+//     }, undefined, function ( error ) {
+//         console.error( error );
+// } );
 
 //light 
 var light = new THREE.HemisphereLight( 0xffffbb, 0x080820, 1 );
 scene.add( light );
 
-cube
+// cube
 const geometry = new THREE.BoxGeometry();
 const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
-camera.position.z = 5;
+// camera.position.z = 5;
 
-// const animate = function () {
-//     // requestAnimationFrame( animate );
+// // const animate = function () {
+// //     // requestAnimationFrame( animate );
 
-//     // cube.rotation.x += 0.01;
-//     // scene.rotation.y += 0.01;
+// //     // cube.rotation.x += 0.01;
+// //     // scene.rotation.y += 0.01;
 
-//     renderer.render( scene, camera );
-// };
+// //     renderer.render( scene, camera );
+// // };
 
-// animate();
+// // animate();
 
-function render() {
+// function render() {
 
-    renderer.render(scene, camera);
-}
-render();
+//     renderer.render(scene, camera);
+// }
+// render();
