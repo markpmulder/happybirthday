640349ff3c155aa674d0689af5b934f619a6308c
Mark Mulder <markpmulder@gmail.com>
2021-03-28 18:01:04 +0200

    background color

--- js/mainpage.js
+++ js/mainpage.js
@@ -7,11 +7,13 @@ const scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 camera.position.set(0, 0, 5000);
 
-
-
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
+
+renderer.setClearColor(0x808080);
+var canvas = renderer.domElement
+document.body.appendChild(canvas);
 
 const geometry = new THREE.BoxGeometry();
 const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
