1f357b6b929cc2a0b67ebced844464b258d24fa6
Mark Mulder <markpmulder@gmail.com>
2021-03-28 16:35:14 +0200

    added back in cube

--- js/mainpage.js
+++ js/mainpage.js
@@ -8,11 +8,18 @@ renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
 const loader = new GLTFLoader();
+//cat
 loader.load( 'assets/cat.glb', function ( gltf ) {
     scene.add( gltf.scene );
 }, undefined, function ( error ) {
     console.error( error );
 } );
+
+//cube
+const geometry = new THREE.BoxGeometry();
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
 
 camera.position.z = 5;
 
