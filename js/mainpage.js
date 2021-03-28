a1b362f289d73271d2f5241443e45fdcf80a3a52
Mark Mulder <markpmulder@gmail.com>
2021-03-28 18:02:51 +0200

    trying camera values

--- js/mainpage.js
+++ js/mainpage.js
@@ -5,7 +5,7 @@ const scene = new THREE.Scene();
 //const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 // camera.position.set(0, 0, 1000);
 var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.set(0, 0, 5000);
+camera.position.set(0, 0, 100000);
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
