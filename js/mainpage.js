bb6bb0256ada2aed8be41c1a99501d30d8f8d574
Mark Mulder <markpmulder@gmail.com>
2021-03-28 17:58:38 +0200

    added different camera

--- js/mainpage.js
+++ js/mainpage.js
@@ -4,7 +4,7 @@ import { GLTFLoader } from './examples/jsm/loaders/GLTFLoader.js';
 const scene = new THREE.Scene();
 //const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 // camera.position.set(0, 0, 1000);
-var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1 1000);
+var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 camera.position.set(0, 0, 5000);
 
 
