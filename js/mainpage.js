8c2ee4a0788e909aa11567e0da75d90a81fb1c59
Mark Mulder <markpmulder@gmail.com>
2021-03-28 16:32:41 +0200

    changed three module path

--- js/mainpage.js
+++ js/mainpage.js
@@ -1,3 +1,4 @@
+import * as THREE from './build/three.module.js';
 import { GLTFLoader } from './examples/jsm/loaders/GLTFLoader.js';
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
