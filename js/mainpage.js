4f78b1a8d3cd2a877bb535fcd4d27652e9383487
Mark Mulder <markpmulder@gmail.com>
2021-03-28 17:39:12 +0200

    democode

--- js/mainpage.js
+++ js/mainpage.js
@@ -1,5 +1,37 @@
 import * as THREE from './build/three.module.js';
 import { GLTFLoader } from './examples/jsm/loaders/GLTFLoader.js';
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+const geometry = new THREE.BoxGeometry();
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+
+camera.position.z = 5;
+
+const animate = function () {
+    requestAnimationFrame( animate );
+
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+
+    renderer.render( scene, camera );
+};
+
+animate();
+
+
+
+
+
+
+
 //init scene and camera
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
@@ -20,8 +52,8 @@ document.body.appendChild( renderer.domElement );
 // } );
 
 //light 
-var light = new THREE.HemisphereLight( 0xffffbb, 0x080820, 1 );
-scene.add( light );
+//var light = new THREE.HemisphereLight( 0xffffbb, 0x080820, 1 );
+//scene.add( light );
 
 // cube
 const geometry = new THREE.BoxGeometry();
@@ -29,7 +61,7 @@ const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
-// camera.position.z = 5;
+camera.position.z = 5;
 
 // // const animate = function () {
 // //     // requestAnimationFrame( animate );
@@ -42,8 +74,8 @@ scene.add( cube );
 
 // // animate();
 
-// function render() {
+function render() {
 
-//     renderer.render(scene, camera);
-// }
-// render();
+    renderer.render(scene, camera);
+}
+render();
