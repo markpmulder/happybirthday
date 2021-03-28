771b91b344917d8c1264bf8a7af974d3e4504c08
Mark Mulder <markpmulder@gmail.com>
2021-03-28 16:38:58 +0200

    try to add in cat

--- js/mainpage.js
+++ js/mainpage.js
@@ -27,7 +27,7 @@ const animate = function () {
     requestAnimationFrame( animate );
 
     // cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
+    gltf.rotation.y += 0.01;
 
     renderer.render( scene, camera );
 };
