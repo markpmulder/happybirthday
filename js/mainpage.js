c25694fabcf0718939016ac83e0600b6aa8a93cb
Mark Mulder <markpmulder@gmail.com>
2021-03-28 17:42:31 +0200

    changed camera pos

--- js/mainpage.js
+++ js/mainpage.js
@@ -3,6 +3,7 @@ import { GLTFLoader } from './examples/jsm/loaders/GLTFLoader.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+camera.position.set(0, 0, 500);
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
