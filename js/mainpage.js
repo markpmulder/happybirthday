8a7ee8b144570d8244f514fe37e09dab87968fb9
Mark Mulder <markpmulder@gmail.com>
2021-03-28 16:29:00 +0200

    wrong path gltf loader

--- js/mainpage.js
+++ js/mainpage.js
@@ -1,4 +1,4 @@
-import { GLTFLoader } from './three/examples/jsm/loaders/GLTFLoader.js';
+import { GLTFLoader } from './examples/jsm/loaders/GLTFLoader.js';
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
